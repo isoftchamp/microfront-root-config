a7cd7ba34a028a9a148aa668bfb2aa0618182325
isoftchamp <qwertroot122@outlook.com>
2024-09-14 13:48:12 +0000

    feat: :rocket: initial

---
+++ src/patrick-root-config.ts
@@ -0,0 +1,30 @@
+import { registerApplication, start } from "single-spa";
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
+} from "single-spa-layout";
+import microfrontendLayout from "./microfrontend-layout.html";
+
+// System.import("rxjs");
+// System.import("rxjs/operators");
+
+const routes = constructRoutes(microfrontendLayout);
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    return System.import(name);
+  },
+});
+const layoutEngine = constructLayoutEngine({ routes, applications });
+
+const parser = new DOMParser();
+const doc = parser.parseFromString(microfrontendLayout, "text/html");
+applications.forEach((app) => {
+  const el = doc.querySelector(`[name="${app.name}"]`);
+  const appProps = el ? el.getAttribute("data-props") : "{}";
+  app.customProps = JSON.parse(appProps);
+  registerApplication(app);
+});
+layoutEngine.activate();
+start();
